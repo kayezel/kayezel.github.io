543651d5d973b5c9b275f069b513e36a4e84b934
kayezel <159698933+kayezel@users.noreply.github.com>
2024-02-11 21:34:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 10;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  var element = document.getElementById("willbe")
+  titleElement.innerHTML = "Yayyy!! :3 <br> ok! I'll pick you up at 7pm so get ready <br> mwa!";
+  buttonsContainer.classList.add("hidden");
+  element.style.display = "none";
+  changeImage("yes");
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "Sure kaba ha?",
+    "ako na toh oh!!",
+    "kayzel pleasee :(",
+    "You're breaking my heart",
+    "wala na warak na aking puso",
+    "Stopp! mag yes kana",
+    "wala ka nang magagawa haha",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
